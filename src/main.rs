311f2506af81a3f0004b609a41fe8194c328e7bd
Samurai_Chia <jyjzone@163.com>
2024-12-08 14:43:10 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = give_ownership_back();
+
+    println!("----------the value of s1 : {s1}---------");
+
+    let s2 = String::from("goodye");
+
+    let s3 = take_and_give_back(s2);
+
+    println!("----------the value of s1 : {s3}---------");
+
+    //
+    println!("----------the value of s1 : {s2}---------");
+
+
+}
+
+fn give_ownership_back ()->String{
+    let some = String::from("Hello");
+    return some
+}
+
+fn take_and_give_back(anything:String)->String{
+    return anything
 }
